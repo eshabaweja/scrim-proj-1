633d7173c690c07ac9f9c9d04b833d3359b7c757
eshabaweja <bawejaesha02@gmail.com>
2022-02-13 12:11:42 +0530

    trying (deploy: gh-pages -b master -d build)

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Main from './components/Main';
+import Navbar from './components/Navbar';
+
+
+function App() {
+  return (
+      <div lassName="container">
+          <Navbar />
+          <Main />
+      </div>
+  )
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Navbar() {
+    return (
+        <nav className="nav-bar">
+            <img src="./logo192.png" />
+            <h3>ReactFacts</h3>
+            <h4>React Course - Project 1</h4>
+        </nav>
+    )
+}
